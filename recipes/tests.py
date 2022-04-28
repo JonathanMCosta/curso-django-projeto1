9c7b20e3207fe228e290120ff0b825cf70f36ac7
JONATHAN MARQUES DA COSTA <jonathanmarquesdacosta@gmail.com>
2022-04-27 22:07:39 -0300

    TDD

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,13 +1,24 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+
+from recipes import views
 
 
 class RecipeURLsTest(TestCase):
-    def test_recipe_the_url_is_correct(self):
+    def test_recipe_home_url_is_correct(self):
         url = reverse('recipes:home')
         self.assertEqual(url, '/')
-        
-        
-    def test_recipe_the_url_is_correct(self):
-        url = reverse('recipes:category', args=[1])
-        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve('/')
+        self.assertIs(view.func, views.home)
